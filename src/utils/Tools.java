256bb6baf58560ac36126898bae3485a6bcf3cca
sebastiancif <sebastian.cifuentes@ing.uchile.cl>
2019-06-06 02:25:44 -0400

    add countingSort

--- src/utils/Tools.java
+++ src/utils/Tools.java
@@ -1,10 +1,12 @@
 package utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 public class Tools {
@@ -63,4 +65,59 @@ public class Tools {
 		return results;
 	}
 	
+	public static int[] countingSort(int[] input, HashMap<Character, Integer> universe, String text) {
+		int n = input.length;
+		int[] b = new int[n];
+		Integer[] integerInput = new Integer[n];
+		HashMap<Character, Integer> c = new HashMap<Character, Integer>();
+		Object[] alphabet = universe.keySet().toArray();
+		for (Character k : universe.keySet() ) {
+			// we initialize all values in c to 0
+			c.put(k, 0);
+		}
+		for(int j = 0; j < n; j++) {
+			c.put(text.charAt(input[j]), c.get(text.charAt(input[j])) + 1);
+			integerInput[j] = new Integer(input[j]);
+		}
+		List<Integer> inputAsList = Arrays.asList(integerInput);
+		for(int i = 1; i < alphabet.length; i++) {
+			c.put((Character) alphabet[i], c.get(alphabet[i]) + c.get(alphabet[i-1]));
+		}
+		for(int j = n-1; j >= 0; j--) {
+			b[c.get(text.charAt(input[j])) - 1] = inputAsList.indexOf(input[j]);
+			c.put(text.charAt(input[j]), c.get(text.charAt(input[j])) - 1);
+		}
+		return b;
+	}
+	public static void main(String[] args) {
+		System.out.println("Veamos si funca");
+		String text = "banbalbbaala";
+		// Get the universe of strings
+		char[] charArray = text.toCharArray();
+		Set<Character> setOfChars = new HashSet<Character>();
+		for (int i = 0; i < charArray.length; i++) {
+			setOfChars.add(charArray[i]);
+		}
+		charArray = null; // GB come here!
+		Iterator<Character> it = setOfChars.iterator();
+		ArrayList<Character> al = new ArrayList<Character>();
+		while(it.hasNext()) {
+			al.add(it.next());
+		}
+		Collections.sort(al);
+		HashMap<Character, Integer> charSet = new HashMap<Character, Integer>();
+		for (int i = 0; i < al.size(); i++) {
+			charSet.put(al.get(i), i);
+		}
+		System.out.println(charSet);
+		int [] input1 = {0,3,6,9};
+		int [] input2 = {1,4,7,10};
+		int [] input3 = {2,5,8,11};
+		int[] res1 = countingSort(input1, charSet, text);
+		int[] res2 = countingSort(input2, charSet, text);
+		int[] res3 = countingSort(input3, charSet, text);
+		System.out.println(Arrays.toString(res1));//3012
+		System.out.println(Arrays.toString(res2));//0123
+		System.out.println(Arrays.toString(res3));//2310
+	}
 }
